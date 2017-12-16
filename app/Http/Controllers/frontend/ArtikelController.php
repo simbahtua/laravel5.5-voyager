55e4a18ccf062e63d18d8f2eafcf9dbe3e740318
yudha wirawan <developer09@esoftdream.net>
2017-12-16 19:49:26 +0700

    update revisi 16 des 2017

--- app/Http/Controllers/frontend/ArtikelController.php
+++ app/Http/Controllers/frontend/ArtikelController.php
@@ -6,35 +6,36 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\facades\DB;
 Use View;
+use App\Carbon\Carbon;
 
-class BeritaController extends Controller {
+class ArtikelController extends Controller {
 
     function index() {
-        $data['page_title'] ='Berita';
-        $data ['data'] = DB::table('posts')
+        $data['page_title'] ='Artikel';
+        
+        $data['data'] = DB::table('posts')
                 ->join('users', 'users.id', '=', 'author_id')
                 ->select('posts.*', 'name')
-//                ->join('*')
-                ->where('category_id', '2')
+                ->where('category_id', '1')
                 ->where('STATUS', 'PUBLISHED')
                 ->get();
 
-        return \View::make('frontend.home.list_berita', $data);
+        return \View::make('frontend.home.list_artikel', $data);
     }
 
-    function detail($id) {  
-        $data['page_title'] ='Detail Berita';
+    function detail($id) {
+        $data['page_title'] ='Detail Artikel';        
         
         $data['data'] = DB::table('posts')
                 ->join('users', 'users.id', '=', 'author_id')
                 ->select('posts.*', 'name')
 //                ->join('*')
-                ->where('posts.category_id', '2')
+                ->where('posts.category_id', '1')
                 ->where('posts.id', $id)
                 ->where('STATUS', 'PUBLISHED')
                 ->first();
 
-        return \View::make('frontend.home.detail_berita', $data);
+        return \View::make('frontend.home.detail_artikel', $data);
     }
 
 }
