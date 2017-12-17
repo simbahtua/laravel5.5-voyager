66bbee080968fca0f73b92b0d0cc237051b234ea
yudha wirawan <developer09@esoftdream.net>
2017-12-17 17:31:01 +0700

    update revisi 17 des 2017

--- resources/views/frontend/home/index.blade.php
+++ resources/views/frontend/home/index.blade.php
@@ -13,41 +13,53 @@
                             <h2>Berita Terkini</h2>
                         </div>
                         <div class="panel-body">
+
                             <ul class="headlines">
+                                @if(!empty($berita))
+                                @foreach ($berita as $row_berita)
+                                @php
+                                $text = substr($row_berita->body, 0, 350);
+                                $date = $row_berita->created_at;
+                                $tanggal = date('l , d M Y', strtotime($date));
+                                @endphp
+
                                 <li>
-                                    <div class="img"><img src="web/uploads/news/news.jpg" alt=""></div>
-                                    <a href="#" class="headline-title" title="article title">Lorem ipsum dolor sit amet, consectetur adipisicing elit</a>
-                                    <span class="meta">Selasa, 5 Desember 2017 5:00</span>
+                                    <div class="img"><img src="<?php echo e(asset('storage/')); ?>/{{$row_berita->image}}" alt="{{ $row_berita->title }}"></div>
+                                    <a href="{{route('detail_berita', [$row_berita->id])}}" class="headline-title" title="{{ $row_berita->title }}">{{ $row_berita->title }}</a>
+                                    <span class="meta">{{ $tanggal }}</span>
                                 </li>
-                                <li>
-                                    <div class="img"><img src="web/uploads/news/news.jpg" alt=""></div>
-                                    <a href="#" class="headline-title" title="article title">Lorem ipsum dolor sit amet, consectetur adipisicing elit</a>
-                                    <span class="meta">Selasa, 5 Desember 2017 5:00</span>
-                                </li>
+                                @endforeach
+                                @else 
+                                <div class="alert alert-info">
+                                    Data Berita Belum Ada 
+                                </div>
+                                @endif
                             </ul>
 
                             <h3 class="clr-heading">Berita Lainnya</h3>
                             <ul class="list-other">
+                                @if(!empty($berita_random))
+                                @foreach ($berita_random as $row_berita_random)
+                                @php
+                                $text = substr($row_berita_random->body, 0, 350);
+                                $date = $row_berita_random->created_at;
+                                $tanggal = date('l , d M Y', strtotime($date));
+                                @endphp
+
                                 <li>
-                                    <a href="#" class="headline-title" title="article title">Lorem ipsum dolor sit amet, consectetur adipisicing elit</a>
-                                    <span class="meta">Selasa, 5 Desember 2017 5:00</span>
+                                    <a href="{{route('detail_berita', [$row_berita_random->id])}}" class="headline-title" title="article title">{{ $row_berita_random->title }}</a>
+                                    <span class="meta">{{ $tanggal }}</span>
                                 </li>
-                                <li>
-                                    <a href="#" class="headline-title" title="article title">Lorem ipsum dolor sit amet, consectetur adipisicing elit</a>
-                                    <span class="meta">Selasa, 5 Desember 2017 5:00</span>
-                                </li>
-                                <li>
-                                    <a href="#" class="headline-title" title="article title">Lorem ipsum dolor sit amet, consectetur adipisicing elit</a>
-                                    <span class="meta">Selasa, 5 Desember 2017 5:00</span>
-                                </li>
-                                <li>
-                                    <a href="#" class="headline-title" title="article title">Lorem ipsum dolor sit amet, consectetur adipisicing elit</a>
-                                    <span class="meta">Selasa, 5 Desember 2017 5:00</span>
-                                </li>
+                                @endforeach
+                                @else 
+                                <div class="alert alert-info">
+                                    Data Berita Belum Ada 
+                                </div>
+                                @endif
                             </ul>
                         </div>
                         <div class="panel-footer">
-                            <a href="#" class="more-link">Berita Selengkapnya &nbsp;&raquo;</a>
+                            <a href="<?php echo url('/list_berita'); ?>" class="more-link">Berita Selengkapnya &nbsp;&raquo;</a>
                         </div>
                     </div>
                 </div>
@@ -61,40 +73,51 @@
                         </div>
                         <div class="panel-body">
                             <ul class="headlines">
+                                @if(!empty($artikel))
+                                @foreach ($artikel as $row_artikel)
+                                @php
+                                $text = substr($row_artikel->body, 0, 350);
+                                $date = $row_artikel->created_at;
+                                $tanggal = date('l , d M Y', strtotime($date));
+                                @endphp
+
                                 <li>
-                                    <div class="img"><img src="web/uploads/news/news.jpg" alt=""></div>
-                                    <a href="#" class="headline-title" title="article title">Lorem ipsum dolor sit amet, consectetur adipisicing elit</a>
-                                    <span class="meta">Selasa, 5 Desember 2017 5:00</span>
+                                    <div class="img"><img src="<?php echo e(asset('storage/')); ?>/{{$row_artikel->image}}" alt="{{ $row_artikel->title }}"></div>
+                                    <a href="{{route('detail_berita', [$row_artikel->id])}}" class="headline-title" title="{{ $row_artikel->title }}">{{ $row_artikel->title }}</a>
+                                    <span class="meta">{{ $tanggal }}</span>
                                 </li>
-                                <li>
-                                    <div class="img"><img src="web/uploads/news/news.jpg" alt=""></div>
-                                    <a href="#" class="headline-title" title="article title">Lorem ipsum dolor sit amet, consectetur adipisicing elit</a>
-                                    <span class="meta">Selasa, 5 Desember 2017 5:00</span>
-                                </li>
+                                @endforeach
+                                @else 
+                                <div class="alert alert-info">
+                                    Data Artikel Belum Ada 
+                                </div>
+                                @endif
                             </ul>
 
                             <h3 class="clr-heading">Artikel Lainnya</h3>
                             <ul class="list-other">
+                                @if(!empty($artikel_random))
+                                @foreach ($artikel_random as $row_artikel_random)
+                                @php
+                                $text = substr($row_artikel_random->body, 0, 350);
+                                $date = $row_artikel_random->created_at;
+                                $tanggal = date('l , d M Y', strtotime($date));
+                                @endphp
+
                                 <li>
-                                    <a href="#" class="headline-title" title="article title">Lorem ipsum dolor sit amet, consectetur adipisicing elit</a>
-                                    <span class="meta">Selasa, 5 Desember 2017 5:00</span>
+                                    <a href="{{route('detail_berita', [$row_artikel_random->id])}}" class="headline-title" title="article title">{{ $row_artikel_random->title }}</a>
+                                    <span class="meta">{{ $tanggal }}</span>
                                 </li>
-                                <li>
-                                    <a href="#" class="headline-title" title="article title">Lorem ipsum dolor sit amet, consectetur adipisicing elit</a>
-                                    <span class="meta">Selasa, 5 Desember 2017 5:00</span>
-                                </li>
-                                <li>
-                                    <a href="#" class="headline-title" title="article title">Lorem ipsum dolor sit amet, consectetur adipisicing elit</a>
-                                    <span class="meta">Selasa, 5 Desember 2017 5:00</span>
-                                </li>
-                                <li>
-                                    <a href="#" class="headline-title" title="article title">Lorem ipsum dolor sit amet, consectetur adipisicing elit</a>
-                                    <span class="meta">Selasa, 5 Desember 2017 5:00</span>
-                                </li>
+                                @endforeach
+                                @else 
+                                <div class="alert alert-info">
+                                    Data Artikel Belum Ada 
+                                </div>
+                                @endif
                             </ul>
                         </div>
                         <div class="panel-footer">
-                            <a href="#" class="more-link">Artikel Selengkapnya &nbsp;&raquo;</a>
+                            <a href="<?php echo url('/list_artikel'); ?>" class="more-link">Artikel Selengkapnya &nbsp;&raquo;</a>
                         </div>
                     </div>
                 </div>
@@ -105,42 +128,55 @@
         <div class="row">
             <div class="col-md-12">
                 <div id="widget-sitelink">
-                    <ul>
-                        <li>
-                            <a href="#" title="Title Link">
-                                <img src="web/images/links/banner-hukum.png" alt="img title">
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" title="Title Link">
-                                <img src="web/images/links/banner-hukum.png" alt="img title">
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" title="Title Link">
-                                <img src="web/images/links/banner-hukum.png" alt="img title">
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" title="Title Link">
-                                <img src="web/images/links/banner-hukum.png" alt="img title">
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" title="Title Link">
-                                <img src="web/images/links/banner-hukum.png" alt="img title">
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" title="Title Link">
-                                <img src="web/images/links/banner-hukum.png" alt="img title">
-                            </a>
-                        </li>
-                    </ul>
+                    <div class="panel panel-default panel-theme">
+                        <div class="panel-heading">
+                            <h2 class="heading-center">Gallery</h2>
+                        </div>
+                        <div class="panel-body">
+                            <ul>
+                                <li>
+                                    <a href="#" title="Title Link">
+                                        <img src="web/images/links/banner-hukum.png" alt="img title">
+                                        <h4>Gallery Album 1</h4>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" title="Title Link">
+                                        <img src="web/images/links/banner-hukum.png" alt="img title">
+                                        <h4>Gallery Album 1</h4>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" title="Title Link">
+                                        <img src="web/images/links/banner-hukum.png" alt="img title">
+                                        <h4>Gallery Album 1</h4>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" title="Title Link">
+                                        <img src="web/images/links/banner-hukum.png" alt="img title">
+                                        <h4>Gallery Album 1</h4>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" title="Title Link">
+                                        <img src="web/images/links/banner-hukum.png" alt="img title">
+                                        <h4>Gallery Album 1</h4>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" title="Title Link">
+                                        <img src="web/images/links/banner-hukum.png" alt="img title">
+                                        <h4>Gallery Album 1</h4>
+                                    </a>
+                                </li>
+                            </ul>
 
-                    <div class="nav-slide">
-                        <span class="nav-prev _1st"></span>
-                        <span class="nav-next _1st"></span>
+                            <div class="nav-slide">
+                                <span class="nav-prev _1st"></span>
+                                <span class="nav-next _1st"></span>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
