e80c70db7dffa2278ad4c05a6582e9465eb101d8
haku <developer08@esoftdream.net>
2017-12-18 16:22:38 +0700

    models

---
+++ app/Models/voyager/AppAgenda.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models\voyager;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
+use TCG\Voyager\Facades\Voyager;
+
+class AppAgenda extends Model
+{
+    public function save(array $options = [])
+    {
+        // If no author has been assigned, assign the current user's id as the author of the post
+        if (!$this->author_id && Auth::user()) {
+            $this->author_id = Auth::user()->id;
+        }
+
+        parent::save();
+    }
+
+    public function authorId()
+    {
+        return $this->belongsTo(Voyager::modelClass('User'), 'author_id', 'id');
+    }
+}
---
+++ app/Models/voyager/User.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\voyager;
+
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+    //
+}
